c441aa8cfc691f23961d7128f186ff185d46b8cf
John Stevenson <john@jr0cket.co.uk>
2019-02-24 14:47:20 +0000

    Using Cheshire to read JSON
    Reading in a JSON file and converting it to a Clojure map using Cheshire.

--- test/my_api/core_test.clj
+++ test/my_api/core_test.clj
@@ -14,3 +14,31 @@
           body     (parse-body (:body response))]
       (is (= (:status response) 200))
       (is (= (:result body) 3)))))
+
+
+;; reading in JSON files
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; slurp will read files into our Clojure code.
+
+#_(slurp "spicy-vegan-pepperoni.json")
+;; => "{\n    \"name\"        : \"Spicy Vegan Pepperoni\",\n    \"size\"        : \"XL\",\n    \"origin\"      : {\n        \"country\" : \"FI\",\n        \"city\"    : \"Tampere\"\n    },\n    \"description\" : \"Healthy and delicious Vegan version of a double pepperoni pizza with some jalapenos to spice it up\"\n}\n"
+
+;; We can use cheshire library to convert the JSON to a Clojure data structure.
+
+#_(cheshire/parse-string
+    (slurp "spicy-vegan-pepperoni.json"))
+;; => {"name" "Spicy Vegan Pepperoni", "size" "XL", "origin" {"country" "PO", "city" "Tampere"}, "description" "Healthy and delicious Vegan version of a double pepperoni pizza with some jalapenos to spice it up"}
+
+
+#_(clojure.pprint/pprint
+    (cheshire/parse-string
+      (slurp "spicy-vegan-pepperoni.json")))
+;; => nil
+
+;; From the REPL output
+;; {"name"   "Spicy Vegitarian Pepperoni",
+;;  "size"   "XL",
+;;  "origin" {"country" "PO", "city" "Tampere"},
+;;  "description"
+;;  "Healthy and delicious Vegitain version of a double pepperoni pizza with some jalapenos to spice it up"}
