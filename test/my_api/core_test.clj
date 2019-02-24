cccc7d91d0f8bb1350acb7278bb6d56ab22f2643
John Stevenson <john@jr0cket.co.uk>
2019-02-24 14:46:54 +0000

    Updated Test documentation

--- test/my_api/core_test.clj
+++ test/my_api/core_test.clj
@@ -7,9 +7,9 @@
 (defn parse-body [body]
   (cheshire/parse-string (slurp body) true))
 
-(deftest a-test
+(deftest adder-test
 
-  (testing "Test GET request to /hello?name={a-name} returns expected response"
+  (testing "Test GET request to /api/plus?x={number}&y={number} returns expected response"
     (let [response (app (-> (mock/request :get  "/api/plus?x=1&y=2")))
           body     (parse-body (:body response))]
       (is (= (:status response) 200))
